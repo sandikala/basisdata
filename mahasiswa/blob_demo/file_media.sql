cf6c30512ec0fcdd040a0331c51afb101c102be0
M Eriyadi <123039241+sandikala@users.noreply.github.com>
2026-09-14 14:47:35 +0700

    Delete mahasiswa directory

--- mahasiswa/blob_demo/file_media.sql
+++
@@ -1,9 +0,0 @@
-USE praktikum_db;
-CREATE TABLE file_media (
-  id        INT AUTO_INCREMENT PRIMARY KEY,
-  nama_file VARCHAR(255) NOT NULL,
-  mime      VARCHAR(100) NOT NULL,
-  ukuran    INT NOT NULL,
-  isi       MEDIUMBLOB NOT NULL,
-  diunggah  TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
